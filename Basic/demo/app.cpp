1bd3a006dc21dc977a81413ee7a41d5ac2decf19
vikram <vikramdev772@gmail.com>
2024-06-12 14:15:03 +0530

    demo: app.cpp

--- Basic/demo/app.cpp
+++ Basic/demo/app.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
 
+void printNumbers(){
+    int n;
+    std::cout<<"\n\t Enter the value of n : ";
+    std::cin>>n;
+    for(int i=1;i<=n;i++){
+        std::cout<<i<<"\n";
+    }
+
+}
+
+
 // Function declaration
 void printName(int n);
 
@@ -9,6 +20,8 @@ int main() {
     std::cin >> x;
     std::cout << "\n\t Value: " << x <<"\n";
     printName(x);
+    //function call
+    printNumbers();
     
     return 0;
 }
@@ -26,3 +39,20 @@ void printName(int n){
 
 
 }
+
+
+// C:\Users\ram77\OneDrive\Desktop\DSA\CPP-DSA\Basic\demo>g++ app.cpp -o app
+
+// C:\Users\ram77\OneDrive\Desktop\DSA\CPP-DSA\Basic\demo>.\app
+
+//          Enter the value: 2
+
+//          Value: 2
+//  ram
+//  ram
+
+//          Enter the value of n : 3
+// 1
+// 2
+// 3
+
