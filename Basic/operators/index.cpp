c171a6135c06c004ecee66c30cbbfb6173efb713
vikram <vikramdev772@gmail.com>
2024-06-13 07:29:52 +0530

    index.cpp

--- Basic/operators/index.cpp
+++ Basic/operators/index.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int sum(int x, int y) {
+int sum(int x, int y)
+{
     return x + y;
 }
+int sub(int a, int b)
+{
+    return a - b;
+}
+int mul(int a, int b)
+{
+    return a * b;
+}
+int d(int x,int y){
+    cout<<"\n\t divison of x of y : "<<x/y<<endl;
+}
 
-int main() {
+int main()
+{
     cout << "\n\t operators " << endl;
     int x, y;
     cout << "\n\t Enter the value x : ";
@@ -13,6 +26,8 @@ int main() {
     cout << "\n\t Enter the value y : ";
     cin >> y;
     cout << "\n\t Sum of x and y is: " << sum(x, y) << endl;
+    cout << "\n\t Sub of x and y is: " << sub(x, y) << endl;
+    cout << "\n\t product of x and y is :" << mul(x, y) << endl;
+    d(x,y);
     return 0;
 }
-
